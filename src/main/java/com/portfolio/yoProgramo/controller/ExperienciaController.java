0132766e2fabe9c564e678da2a50e3908349f7fd
catul0 <valesigaudo1@gmail.com>
2023-02-16 23:24:38 -0300

    url actualizadas de local a produccion

--- src/main/java/com/portfolio/yoProgramo/controller/ExperienciaController.java
+++ src/main/java/com/portfolio/yoProgramo/controller/ExperienciaController.java
@@ -21,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("experiencia")
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins={"portfolio-valentin-frontend.web.app","http://localhost:4200"})
 public class ExperienciaController {
     @Autowired
     ExperienciaService sExperiencia;
--- src/main/java/com/portfolio/yoProgramo/controller/HabilidadesController.java
+++ src/main/java/com/portfolio/yoProgramo/controller/HabilidadesController.java
@@ -21,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("habilidades")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins={"portfolio-valentin-frontend.web.app","http://localhost:4200"})
+
 public class HabilidadesController {
 
     @Autowired
